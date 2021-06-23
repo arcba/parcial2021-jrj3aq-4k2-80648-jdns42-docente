25166274936f6b684c56b24b61ec84e67fa111eb
arcba <alejandroreyc@yahoo.com.ar>
2021-06-23 16:41:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# parcial2021-jrj3aq-4k2-80648-jdns42-docente
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/parcial2021-jrj3aq-4k2-80648-jdns42-docente)
---
+++ src/app/components/productos/productos.component.ts
@@ -0,0 +1,111 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalDialogService } from '../../services/modal-dialog.service';
+import { Producto } from '../../models/producto';
+import { ProductosService } from '../../services/productos.service';
+
+@Component({
+  selector: 'app-productos',
+  templateUrl: './productos.component.html',
+  styleUrls: ['./productos.component.css']
+})
+export class ProductosComponent implements OnInit {
+  Titulo = 'Productos';
+  TituloAccionABMC = {
+    A: '(Agregar)',
+    B: '(Eliminar)',
+    M: '(Modificar)',
+    C: '(Consultar)',
+    L: '(Listado)'
+  };
+  AccionABMC = 'L'; // inicialmente inicia en el listado de articulos (buscar con parametros)
+
+  Items: Producto[] = null;
+  submitted: boolean = false;
+
+  FormRegistro: FormGroup;
+
+  constructor(
+    public formBuilder: FormBuilder,
+    private productosService: ProductosService,
+    private modalDialogService: ModalDialogService
+  ) {}
+
+  ngOnInit() {
+    this.FormRegistro = this.formBuilder.group({
+      IdArticulo: [null],
+      ProductoNombre: [
+        null,
+        [Validators.required, Validators.minLength(1), Validators.maxLength(50)]
+      ],
+      ProductoStock: [
+        null,
+        [Validators.required, Validators.pattern('^\\d{1,10}$')]
+      ],
+      ProductoFechaAlta: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern(
+            '(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}'
+          )
+        ]
+      ]
+    });
+  }
+
+  Agregar() {
+    this.AccionABMC = 'A';
+    this.FormRegistro.reset({ Activo: true, IdArticulo: 0 });
+    this.submitted = false;
+    this.FormRegistro.markAsUntouched();
+  }
+
+  // Buscar segun los filtros, establecidos en FormRegistro
+  Buscar() {
+    //this.modalDialogService.BloquearPantalla();
+    this.productosService
+      .get()
+      //this.articulosService.get("", null, this.Pagina)
+      .subscribe((res: any) => {
+        this.Items = res;
+        //this.modalDialogService.DesbloquearPantalla();
+      });
+  }
+
+  // grabar tanto altas como modificaciones
+  Grabar() {
+    this.submitted = true;
+    if (this.FormRegistro.invalid) {
+      return;
+    }
+
+    //hacemos una copia de los datos del formulario, para modificar la fecha y luego enviarlo al servidor
+    const itemCopy = { ...this.FormRegistro.value };
+
+    //convertir fecha de string dd/MM/yyyy a ISO para que la entienda webapi
+    var arrFecha = itemCopy.ProductoFechaAlta.substr(0, 10).split('/');
+    if (arrFecha.length == 3)
+      itemCopy.ProductoFechaAlta = new Date(
+        arrFecha[2],
+        arrFecha[1] - 1,
+        arrFecha[0]
+      ).toISOString();
+
+    // agregar post
+    if (this.AccionABMC == 'A') {
+      //this.modalDialogService.BloquearPantalla();
+      this.productosService.post(itemCopy).subscribe((res: any) => {
+        this.Volver();
+        this.modalDialogService.Alert('Registro agregado correctamente.');
+        this.Buscar();
+        //this.modalDialogService.DesbloquearPantalla();
+      });
+    }
+  }
+
+  // Volver desde Agregar/Modificar
+  Volver() {
+    this.AccionABMC = 'L';
+  }
+}
